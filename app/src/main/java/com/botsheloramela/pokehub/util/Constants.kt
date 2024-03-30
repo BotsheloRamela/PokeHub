8eaf00459784865b0f7df02ae73dd686c450ab5e
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-03-30 21:54:22 +0200

    Add Pokemon parser util & update POKEMON_IMAGE_URL_BASE

--- app/src/main/java/com/botsheloramela/pokehub/util/Constants.kt
+++ app/src/main/java/com/botsheloramela/pokehub/util/Constants.kt
@@ -3,5 +3,5 @@ package com.botsheloramela.pokehub.util
 object Constants {
     const val BASE_URL = "https://pokeapi.co/api/v2/"
     const val PAGE_SIZE = 20
-    const val POKEMON_IMAGE_URL_BASE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+    const val POKEMON_IMAGE_URL_BASE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/"
 }
